7d53f53f7c3442df4e9545165a8d55f24984a659
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2023-10-21 20:42:36 +0700

    Update

---
+++ src/test/java/org/huy/steps/GoogleStepsDefs.java
@@ -0,0 +1,10 @@
+package org.huy.steps;
+
+import io.cucumber.java.en.Given;
+
+public class GoogleStepsDefs {
+    @Given("Open browser and navigate to google search url")
+    public void open_browser_and_navigate_to_google_search_url() {
+
+    }
+}
---
+++ src/test/resources/feature/Google_Search.feature
@@ -0,0 +1,5 @@
+@Google
+Feature: Google search
+
+  Scenario: Open google search url and verify can search by text
+    Given Open browser and navigate to google search url
