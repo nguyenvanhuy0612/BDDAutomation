8265a4c72338f61ecab2164a5169edaf1c111508
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2023-10-21 20:51:07 +0700

    Update

--- src/test/java/org/huy/runner/TestRunner.java
+++ src/test/java/org/huy/runner/TestRunner.java
@@ -5,11 +5,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.FEATURES_PROPERTY_NAME;
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("resources/test/feature")
+@SelectClasspathResource("resources")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.huy.steps")
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "resources/feature")
 public class TestRunner {
 }
